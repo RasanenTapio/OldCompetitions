9e24b581ecf454d320fe3b45fd4390dab64b1e30
Tapio Räsänen <wandrw4@gmail.com>
2015-09-02 22:21:22 +0300

    h2o loop

--- mutual/mutual_h2o.r
+++ mutual/mutual_h2o.r
@@ -1,101 +1,150 @@
 setwd("C:/mutualdata")
 library(h2o)
+library(plyr)
 
-# remember to set seed
+set.seed(10)
 
+# Load data
 train <- read.csv("mutualdata/train.csv", header = TRUE)
 test <- read.csv("mutualdata/test.csv", header = TRUE)
-results <- read.csv("sample_submission.csv", header = TRUE)
 
-# drop id
+# Drop id
 train <- train[, -1]
 
-for(i in 2:dim(train)[2]) {
-	train[,i] <- as.factor(train[,i])
-}
+#for(i in 2:dim(train)[2]) {
+#	train[,i] <- as.factor(train[,i])
+#}
 
-for(i in 2:dim(test)[2]) {
-	test[,i] <- as.factor(test[,i])
-}
+#for(i in 2:dim(test)[2]) {
+#	test[,i] <- as.factor(test[,i])
+#}
+
+# Or this:
+train$T1_V4<-as.factor(train$T1_V4)
+train$T1_V5<-as.factor(train$T1_V5)
+train$T1_V6<-as.factor(train$T1_V6)
+train$T1_V7<-as.factor(train$T1_V7)
+train$T1_V8<-as.factor(train$T1_V8)
+train$T1_V9<-as.factor(train$T1_V9)
+train$T1_V11<-as.factor(train$T1_V11)
+train$T1_V12<-as.factor(train$T1_V12)
+train$T1_V15<-as.factor(train$T1_V15)
+train$T1_V16<-as.factor(train$T1_V16)
+train$T1_V17<-as.factor(train$T1_V17)
+train$T2_V3<-as.factor(train$T2_V3)
+train$T2_V5<-as.factor(train$T2_V5)
+train$T2_V11<-as.factor(train$T2_V11)
+train$T2_V12<-as.factor(train$T2_V12)
+train$T2_V13<-as.factor(train$T2_V13)
+
+test$T1_V4<-as.factor(test$T1_V4)
+test$T1_V5<-as.factor(test$T1_V5)
+test$T1_V6<-as.factor(test$T1_V6)
+test$T1_V7<-as.factor(test$T1_V7)
+test$T1_V8<-as.factor(test$T1_V8)
+test$T1_V9<-as.factor(test$T1_V9)
+test$T1_V11<-as.factor(test$T1_V11)
+test$T1_V12<-as.factor(test$T1_V12)
+test$T1_V15<-as.factor(test$T1_V15)
+test$T1_V16<-as.factor(test$T1_V16)
+test$T1_V17<-as.factor(test$T1_V17)
+test$T2_V3<-as.factor(test$T2_V3)
+test$T2_V5<-as.factor(test$T2_V5)
+test$T2_V11<-as.factor(test$T2_V11)
+test$T2_V12<-as.factor(test$T2_V12)
+test$T2_V13<-as.factor(test$T2_V13)
+
+train<-as.data.frame(train)
+test<-as.data.frame(test)
 
 # if sample:
-otos <- sample(1:dim(train)[1], dim(train)[1]*0.1)
+#otos <- sample(1:dim(train)[1], dim(train)[1]*0.1)
 
 # Hazard or log+1 hazard?
 train$Hazard <- log(train$Hazard + 1)
-# kokeile 1/16 root
-train$Hazard <- (train$Hazard)^(1/16)
 
-trainsmall <- train[-otos,]
-valid <- train[otos,]
+train_a <- split(train, sample(rep(1:2, dim(train)[1]))) # try sampling with replacement
+train1 <- train_a[[1]]
+train2 <- train_a[[2]]
+
+
+# kokeile 1/16 root
+#train$Hazard <- (train$Hazard)^(1/16)
+
+#trainsmall <- train[-otos,]
+#valid <- train[otos,]
 # cluster
 localH2O <- h2o.init(nthread=4,Xmx="10g")
 
-h2o_train <- as.h2o(localH2O,trainsmall)
-h2o_valid <- as.h2o(localH2O,valid)
 h2o_test <- as.h2o(localH2O,test)
-h2o_trainfull <- as.h2o(localH2O,train)
 
+# Parameters
 response <- 1
 predictors <- 2:33
+looppeja <- 10
+
+# Create results frame
+results <- read.csv("sample_submission.csv", header = TRUE)
+append_results1 <- results
+append_results2 <- results
 
 # Some models (Gradient boosted trees)
+for(i in 1:looppeja){
 
-model6 <- h2o.gbm(y = response, x = predictors,
-	data = h2o_train, validation=h2o_valid,
-	distribution = "gaussian",
-	n.tree = 700, shrinkage = 0.02, interaction.depth = 8)
+	# split data
+	train_a <- split(train, sample(rep(1:2, dim(train)[1]))) # try sampling with replacement
+	train1 <- train_a[[1]]
+	train2 <- train_a[[2]]
+	# or 2 samples with or withot replacement
 	
-model7 <- h2o.gbm(y = response, x = predictors,
-	data = h2o_train, validation=h2o_valid,
-	distribution = "gaussian",
-	n.tree = 700, shrinkage = 0.02, interaction.depth = 8)
-	
-# 351, c(10,8,11,7), 0.02)
+	h2o_train1 <- as.h2o(localH2O,train1)
+	h2o_train2 <- as.h2o(localH2O,train2)
 
-	# family = gaussian?
-	# importance = T) ja max depth 9?
+	model6 <- h2o.gbm(y = response, x = predictors,
+		data = h2o_train1,
+		distribution = "gaussian",
+		n.tree = 150, shrinkage = 0.2, interaction.depth = 15)
+		
+	model7 <- h2o.gbm(y = response, x = predictors,
+		data = h2o_train2,
+		distribution = "gaussian",
+		n.tree = 150, shrinkage = 0.2, interaction.depth = 15)
 
-model7x <- h2o.deeplearning(y = response, x = predictors,
-	data = h2o_train, validation=h2o_valid,
-	epochs = 20, hidden = c(200,200,200)
-	classification = FALSE)
+	#### Loop over and append to data frame
+		
+	## Prediction
+	append_results1$Hazard <- as.data.frame(h2o.predict(model6,h2o_test))$predict
+	append_results2$Hazard <- as.data.frame(h2o.predict(model7,h2o_test))$predict
 
-# Deep learning model:
-model_deep1 <- h2o.deeplearning(x=predictors,
-                          y=response,
-                          data=h2o_trainfull,
-						  hidden=c(199,90),
-                          epochs=50, activation="Rectifier",
-                          classification=FALSE,
-						  rho=0.99,
-						  epsilon=1e-8)
-# 3 best models without log transformation: rho, epsilin: (0.99,1.0E-6) (0.99,1.0E-8) (0.95,1.0E-6)
-# 3 best models with log+1 transformation: rho, epsilin: (0.999,1.0E-8) (0.99,1.0E-8) (0.9,1.0E-6)
+	# if first
+	if (i == 1) {
+		results <- rbind(append_results1, append_results2)
+	} else
+		results <- rbind(results, append_results1, append_results2)
+}
 
-## Prediction
-prediction1 <- as.data.frame(h2o.predict(model6,h2o_test))
-prediction2 <- as.data.frame(h2o.predict(model7,h2o_test))
 
-results$Hazard <- prediction2$predict
+# use aggregate to get mean or median for for every id
+aggdata_mean <- aggregate(Hazard ~ Id, results, "mean")
+aggdata_median <- aggregate(Hazard ~ Id, results, "median")
 
-head(results)
-mean(train$Hazard); mean(results$Hazard); sd(train$Hazard); sd(results$Hazard)
+head(aggdata_mean)
+mean(train$Hazard); mean(aggdata_mean$Hazard); sd(train$Hazard); sd(aggdata_mean$Hazard)
 
 # Plot two overlaying histograms
-plot1 <- density(results$Hazard)
+plot1 <- density(aggdata_median$Hazard)
 plot2 <- density(train$Hazard)
 plot(plot1, col=rgb(0,0,1,1/4), xlim=c(0,15), main = "Hazard")
 lines(plot2, col=rgb(1,0,0,1/4), xlim=c(0,15))
 
 # Sava results
-results$Hazard <- (exp(prediction1$predict)-1)*0.50 + ((prediction2$predict)^16)*0.50
-#results$Hazard <- (results$Hazard)^16
-sub_name <- "gbm_t700_d8_rate02_log1_power16"
-sub_file <- results
-head(results)
+submission <- read.csv("sample_submission.csv", header = TRUE)
+submission$Hazard <- exp(aggdata_median$Hazard) - 1
+sub_name <- "gbm_t150_d15_rate20_log1_m20_mean"
+head(submission)
 
-write.table(sub_file, file = paste('results/', sub_name,'.csv', sep = ""), row.names = FALSE,
+write.table(submission, file = paste('results/', sub_name,'.csv', sep = ""), row.names = FALSE,
 	quote = FALSE, col.names = TRUE, sep=",")
 
-# h2o.shutdown(localH2O)
+# Shut down	
+h2o.shutdown(localH2O)
