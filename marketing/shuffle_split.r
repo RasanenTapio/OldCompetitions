0fa8f9802d712ba453b95984077423caf6bd58cc
Tapio Räsänen <wandrw4@gmail.com>
2015-09-18 00:12:15 +0300

    Added date features
    Added features from timestamp and date variables.

--- marketing/shuffle_split.r
+++ marketing/shuffle_split.r
@@ -6,7 +6,7 @@ library(data.table)
 
 # Parameters
 make_splits <- 2
-output_dir <- "weight2a"
+output_dir <- "featuredate2"
 
 # Working directory
 setwd("C:/marketingdata")
@@ -17,53 +17,72 @@ Sys.setlocale("LC_TIME", "English")
 
 # Load data # improve this with: na=c("",NA,-1,9999,999))
 train <- fread("C:/marketingdata/data/train.csv",
-	na.strings=c("NA","N/A","", "999", "9999"))
+	na.strings=c("NA","N/A","", "999", "9999"),stringsAsFactors=FALSE)
 test <- fread("C:/marketingdata/data/test.csv",
-	na.strings=c("NA","N/A","", "999", "9999"))
+	na.strings=c("NA","N/A","", "999", "9999"),stringsAsFactors=FALSE)
 
-### Date values: Set df$day <- weekdays(as.Date(df$date))
-### cols:
+#train.unique.count=lapply(train, function(x) length(unique(x)))
+#train.unique.count_1=unlist(train.unique.count[unlist(train.unique.count)==1])
+#train.unique.count_2=unlist(train.unique.count[unlist(train.unique.count)==2])
+#train.unique.count_2=train.unique.count_2[-which(names(train.unique.count_2)=='target')]
+
+#delete_const=names(train.unique.count_1)
+#delete_NA56=names(which(unlist(lapply(train[,(names(train) %in% names(train.unique.count_2))], function(x) max(table(x,useNA='always'))))==145175))
+#delete_NA89=names(which(unlist(lapply(train[,(names(train) %in% names(train.unique.count_2))], function(x) max(table(x,useNA='always'))))==145142))
+#delete_NA918=names(which(unlist(lapply(train[,(names(train) %in% names(train.unique.count_2))], function(x) max(table(x,useNA='always'))))==144313))
+
+#VARS to delete
+#safe to remove VARS with 56, 89 and 918 NA's as they are covered by other VARS
+#print(length(c(delete_const,delete_NA56,delete_NA89,delete_NA918)))
+
+#train=train[,!(names(train) %in% c(delete_const,delete_NA56,delete_NA89,delete_NA918)),with = FALSE]
+#test=test[,!(names(test) %in% c(delete_const,delete_NA56,delete_NA89,delete_NA918)),with = FALSE]
+
+### Date and timestapm values:
+
 datecolumns = c("VAR_0073", "VAR_0075", "VAR_0156", "VAR_0157", "VAR_0158",
 	"VAR_0159", "VAR_0166", "VAR_0167", "VAR_0168", "VAR_0176", "VAR_0177",
 	"VAR_0178", "VAR_0179","VAR_0217")
-#timestampcolumns <- "VAR_0204"
-# datecolumns <- c("VAR_0073", "VAR_0075")
-# timestampcolumns <- "VAR_0204"
 
 # apply for timestamp columns
 gettimestamp <- function(var1, var_name = "X1") {
 	gettimestamp <- strptime(var1, format="%d%b%y:%H:%M:%S")
-	frame1 <- data.frame(new_wd = substr(weekdays(gettimestamp), 1, 3))
+	frame1 <- data.frame(new_wd = as.character(substr(weekdays(gettimestamp), 1, 3)), stringsAsFactors=FALSE)
 	frame1$new_hour = hour(gettimestamp)
 	frame1$new_month = month(gettimestamp)
 	frame1$new_year = year(gettimestamp)
-	names(frame1) <- paste(var_name,c("wd", "hour", "month", "year"),sep = "_")
+	frame1$numdate = as.double(as.Date(gettimestamp))
+	names(frame1) <- paste(var_name,c("wd", "hour", "month", "year","date"),sep = "_")
 	return(frame1)
 }
 
 getdate <- function(var1, var_name = "X1") {
 	gettimestamp <- strptime(var1, format="%d%b%y:%H:%M:%S")
-	frame1 <- data.frame(new_wd = substr(weekdays(gettimestamp), 1, 3))
+	frame1 <- data.frame(new_wd = as.character(substr(weekdays(gettimestamp), 1, 3)), stringsAsFactors=FALSE)
 	frame1$new_month = month(gettimestamp)
 	frame1$new_year = year(gettimestamp)
-	names(frame1) <- paste(var_name,c("wd", "month", "year"),sep = "_")
+	frame1$numdate = as.double(as.Date(gettimestamp)) # to count difference of days
+	names(frame1) <- paste(var_name,c("wd", "month", "year","date"),sep = "_")
 	return(frame1)
 }
 
-train <- cbind(train,gettimestamp(train$VAR_0204, var_name = "X1"))
-test <-  cbind(test,gettimestamp(test$VAR_0204, var_name = "X1"))
+train <- cbind(train,gettimestamp(train$VAR_0204, var_name = "VAR_0204"))
+test <-  cbind(test,gettimestamp(test$VAR_0204, var_name = "VAR_0204"))
 
 for (datecolumn in datecolumns) {
-	test <- cbind(test,getdate(as.vector(test[,which( colnames(test)==datecolumn ), with = FALSE]), var_name = datecolumn))
-	#train <- cbind(train,getdate(train[,datecolumn, with = FALSE], var_name = datecolumn))
-	print(datecolumn)
+	train <- cbind(train, getdate(train[[datecolumn]], var_name = datecolumn))
+	test <- cbind(test, getdate(test[[datecolumn]], var_name = datecolumn))
 }
-#tail(test[,datecolumns,with=FALSE])
-#tail(names(test)) -> nim
-#tail(test[,nim,with=FALSE])
-gettimestamp <- strptime(test[,which( colnames(test)=="VAR_0075" ), with = FALSE], format="%d%b%y:%H:%M:%S")
 
-# combine hour to continent / city variable?
+# Drop datecolumns and VAR_0204
+train <- train[, !c(datecolumns,"VAR_0204"), with=FALSE]
+test <- test[, !c(datecolumns,"VAR_0204"), with=FALSE]
+
+# Add more features:
+
+	# Combine hour to continent / city variable? -- dimension reduction
+	# Impute missing values for 4-10 most important variables
+	# New variables: Differences between time variables (date)
 
 # Correct missing values and modify variables
 feature.names <- names(train)[2:ncol(train)]
@@ -72,16 +91,15 @@ feature.names <- feature.names[-which(feature.names == "target")] # keep target
 #### ADDITION: #SKIP THIS?
 cat("assuming text variables
  are categorical & replacing them with numeric ids\n")
-#categorical <- vector()
+
 for (f in feature.names) {
   if (class(train[[f]])=="character") {
     levels <- unique(c(train[[f]], test[[f]]))
     train[[f]] <- as.integer(factor(train[[f]], levels=levels))
     test[[f]]  <- as.integer(factor(test[[f]],  levels=levels))
-	#categorical <- c(categorical,f)
   }
 }
-#categorical # print categorical variable names or numbers
+
 #### ADDITION:
 cat("replacing missing values with -1\n")
 train[is.na(train)] <- -1
@@ -95,25 +113,18 @@ names(train) <- c("X0","X1")
 sample0 <- sample(dim(train$X0)[1], size = dim(train$X0)[1])
 sample1 <- sample(dim(train$X1)[1], size = dim(train$X1)[1])
 
-if (make_splits == 3) {
-	sample0_train <- sample0[1:floor(length(sample0)*0.7)]
-	# stack
-	sample0_valid <- sample0[(floor(length(sample0)*0.7)+1):length(sample0)]
-	sample1_train <- sample1[1:floor(length(sample1)*0.7)]
-	# stack
-	sample1_valid <- sample1[(floor(length(sample1)*0.7)+1):length(sample1)]
-} else {
-	sample0_train <- sample0[1:floor(length(sample0)*0.85)]
-	sample0_valid <- sample0[(floor(length(sample0)*0.85)+1):length(sample0)]
-	sample1_train <- sample1[1:floor(length(sample1)*0.85)]
-	sample1_valid <- sample1[(floor(length(sample1)*0.85)+1):length(sample1)]
-}
+sample0_train <- sample0[1:floor(length(sample0)*0.90)]
+sample0_valid <- sample0[(floor(length(sample0)*0.90)+1):length(sample0)]
+sample1_train <- sample1[1:floor(length(sample1)*0.90)]
+sample1_valid <- sample1[(floor(length(sample1)*0.90)+1):length(sample1)]
+
 # Compare this to results
 table(train$X0$target)
 table(train$X1$target)
 
 # Save 2-3 datasets to output_dir
 
+# Shuffle train to be able to split with h2o:
 valid <- rbind(train$X1[sample1_valid,],train$X0[sample0_valid,])
 train <-rbind(train$X1[sample1_train,],train$X0[sample0_train,])
 head(train$ID)
@@ -123,6 +134,8 @@ head(train$ID)
 # Compare to previous
 table(train$target) + table(valid$target)
 
+# Write sets
+
 write.table(test, file = paste(output_dir,'/test.csv', sep = ""),
 	row.names = FALSE, quote = FALSE, col.names = TRUE, sep=",")
 
@@ -131,5 +144,5 @@ write.table(train, file = paste(output_dir,'/train.csv', sep = ""),
 	
 write.table(valid, file = paste(output_dir,'/valid.csv', sep = ""),
 	row.names = FALSE, quote = FALSE, col.names = TRUE, sep=",")
+print("Saving datasets done")
 print("done")
-print("really")
